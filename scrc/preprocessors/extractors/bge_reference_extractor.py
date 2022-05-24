c25633cd23e09c59e9399df3039f9f8e808b166f
Stern5497 <62178719+Stern5497@users.noreply.github.com>
2022-05-24 11:13:49 +0200

    add abstract methods disable save date since needed fields in database are not created yet change select_df method, similar as it is used in court_composition_extractor

--- scrc/preprocessors/extractors/bge_reference_extractor.py
+++ scrc/preprocessors/extractors/bge_reference_extractor.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
-from typing import Any, TYPE_CHECKING
-
+from typing import Any, TYPE_CHECKING, Union
+import bs4
 import pandas as pd
 from sqlalchemy.engine.base import Engine
 from sqlalchemy.sql.expression import text
@@ -33,6 +33,40 @@ class BgeReferenceExtractor(AbstractExtractor):
             'processing_one': 'Extracting the court composition from',
             'no_functions': 'Not extracting the court compositions.'
         }
+
+    def get_database_selection_string(self, spider: str, lang: str) -> str:
+        """Returns the `where` clause of the select statement for the entries to be processed by extractor"""
+        return f"spider='{spider}' AND header IS NOT NULL AND header <> ''"
+
+    def get_required_data(self, series: pd.DataFrame) -> Union[bs4.BeautifulSoup, str, None]:
+        """Returns the data required by the processing functions"""
+        return {Section.HEADER: series['header']}
+
+    def select_df(self, engine: str, spider: str) -> str:
+        """Returns the `where` clause of the select statement for the entries to be processed by extractor"""
+        only_given_decision_ids_string = f" AND {where_decisionid_in_list(self.decision_ids)}" if self.decision_ids is not None else ""
+        # select analog zur Abfrage in court_composition_extractor aber ohne footer
+        return self.select(engine, f"section headersection {join_decision_and_language_on_parameter('decision_id', 'headersection.decision_id')} {join_file_on_decision()}", f"headersection.decision_id, headersection.section_text as header, '{spider}' as spider, iso_code as language, html_url", where=f"headersection.section_type_id = 1 AND headersection.decision_id IN {where_string_spider('decision_id', spider)} {only_given_decision_ids_string}", chunksize=self.chunksize)
+
+    def save_data_to_database(self, df: pd.DataFrame, engine: Engine):
+        # TODO add table to postgres
+       """
+        for idx, row in df.iterrows():
+            bge_reference = row['bge_reference']
+            with engine.connect() as conn:
+                # TODO first add bge_reference in database
+                t = Table('bge_reference', MetaData(), autoload_with=conn)
+                # delete and reinsert
+                stmt = t.delete().where(delete_stmt_decisions_with_df(df))
+                conn.execute(stmt)
+                stmt = t.insert().values({"decision_id": str(row['decision_id']), "bge_reference": str(row['bge_reference'])})
+                conn.execute(stmt)
+"""
+
+    def check_condition_before_process(self, spider: str, data: Any, namespace: dict) -> bool:
+        """Override if data has to conform to a certain condition before processing.
+        e.g. data is required to be present for analysis"""
+        return bool(data)
 
 
 if __name__ == '__main__':
